c5dc9501e8cdc225a01011db71e888678a317b51
Leandro Boeing Vieira <leandro.vieira@softplan.com.br>
2021-03-31 10:15:30 -0300

    First commit

---
+++ src/main/java/com/jobsity/bowlingscoreboard/Application.java
@@ -0,0 +1,8 @@
+package com.jobsity.bowlingscoreboard;
+
+public class Application {
+    
+	public static void main(String[] args) {
+		System.out.println("Application");
+	}
+}
---
+++ src/test/java/com/jobsity/bowlingscoreboard/ApplicationTest.java
@@ -0,0 +1,15 @@
+/*
+ */
+package com.jobsity.bowlingscoreboard;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+	
+    @Test
+    public void testSetup() {
+        assertNotNull(new Application());
+    }
+}
